9bea246d815b8d700f2f9505427446fd1998ed48
ErickyAll <erick227.ea21@gmail.com>
2022-08-04 22:46:27 -0300

    Add build

---
+++ dist/game-mania/assets/js/login.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  //mask
+  $("#phone").mask("(00) 0 0000-0000");
+
+  //validar form
+  $("#signin").click(function () {
+    let user = $("#usuario").val();
+    let phone = $("#phone").val();
+    let email = $("#email").val();
+    let key = $("#password").val();
+
+    if (
+      user.trim() == "" ||
+      phone.trim() == "" ||
+      email.trim() == "" ||
+      key.trim() == ""
+    ) {
+      alert("Favor, preencher todos os campos");
+    } else {
+      $("#signin").css("background-color", "green");
+    }
+  });
+});
---
+++ dist/game-mania/assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('.menu-bars').click(function () {
+    $('.menu').toggleClass('mostraEscondeMenu')
+  })
+
+  $('.btn-price').click(function () {
+    alert('Produto indisponivel :/')
+  })
+})
+
+// function abreFechaMenu() {
+//   // navbar -tag nav
+//   let navbar = document.querySelector('.menu')
+//   navbar.classList.toggle('mostraEscondeMenu')
+//   // console.log(navbar);
+// }
