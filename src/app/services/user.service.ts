7319baebefe168993c79e6c9eff8fc072f05cc8d
ErickyAll <erick227.ea21@gmail.com>
2022-08-12 22:46:57 -0300

    Sign up

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,24 +4,18 @@ import { Observable } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
-
-  url = " http://localhost:3000/signin"
+  url = ' http://localhost:3000/signin';
 
   //post
-  signin(user: User):Observable<any>{
-    return this.httpClient.post(
-      this.url, 
-      JSON.stringify(user),
-       {
-        headers: new HttpHeaders({"COntent-type": "application/json"}),
-        observe: 'response'
-       })
+  signin(user: User): Observable<any> {
+    return this.httpClient.post(this.url, JSON.stringify(user), {
+      headers: new HttpHeaders({ 'COntent-type': 'application/json' }),
+      observe: 'response',
+    });
   }
 }
