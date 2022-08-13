84bcf9e79a093df43aab320fd7eb0c562cfefc6a
ErickyAll <erick227.ea21@gmail.com>
2022-08-12 22:20:42 -0300

    Validate Login

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,12 +1,7 @@
 export class User {
-
-
   constructor(
     public usuario?: string,
-    public phone?: number,
     public email?: string,
-    public password?: string,
-  ){
-
-  }
+    public password?: string
+  ) {}
 }
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -15,20 +15,39 @@ export class LoginComponent implements OnInit {
   userModel = new User();
   message: string = '';
 
+  validateLogin(): boolean {
+    if (
+      this.userModel.usuario === undefined ||
+      this.userModel.usuario === '' ||
+      this.userModel.email === undefined ||
+      this.userModel.email === '' ||
+      this.userModel.password === undefined ||
+      this.userModel.password === ''
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   //Função de Login
   signin() {
-    // console.log(this.userModel);
+    if (this.validateLogin()) {
+      console.log(this.userModel);
 
-    this.userService.signin(this.userModel).subscribe(
-      //utilizando subscribe seguindo a biblioteca rxJS
-      {
-        next: (response) => {
-          this.message = 'Logado com Sucesso!';
-        },
-        error: (erro) => {
-          this.message = ` ${erro.error} ${erro.status} ${erro.statusText}`;
-        },
-      }
-    );
+      this.userService.signin(this.userModel).subscribe(
+        //utilizando subscribe seguindo a biblioteca rxJS
+        {
+          next: (response) => {
+            this.message = 'Logado com Sucesso!';
+          },
+          error: (erro) => {
+            this.message = ` ${erro.error} ${erro.status} ${erro.statusText}`;
+          },
+        }
+      );
+    } else {
+      this.message = 'Preencha todos os campos';
+    }
   }
 }
